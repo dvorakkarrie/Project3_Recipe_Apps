e5e3c3b289ed631dda37e3d9569f42e7464b5acf
Karrie Dvorak <dvorak.karrie@gmail.com>
2020-03-20 14:35:24 -0500

    set up User model

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,7 +3,14 @@ const mongoose = require('../db/connection');
 
 const UserSchema = new mongoose.Schema({
   email: String,
-  name: String
+  firstName: String,
+  lastName: String,
+  favorites: [
+    {
+      ref: "Recipe",
+      type: mongoose.Schema.Types.ObjectId
+    }
+  ]
 });
 
 const User = mongoose.model('User', UserSchema);
