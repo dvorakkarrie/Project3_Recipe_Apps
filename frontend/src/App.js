f6a66710f11532df89c1e9904671eb14268c84b7
Karrie Dvorak <dvorak.karrie@gmail.com>
2020-03-21 13:33:56 -0500

    created components per wireframe

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';                   // importing component
+import {Redirect, Route, Switch} from 'react-router-dom';   // importing router 
+import axios from 'axios';                                  // importing axios
+
+import './App.css';                                         // importing css file
+
+import Home from './Components/Home';                        // importing Home component
+import User from './Components/User';                        // importing User component
+import NewRecipe from './Components/NewRecipe';              // importing NewRecipe component
+import EditRecipe from './components/EditRecipe';            // importing EditRecipe component
 
 function App() {
   return (
-    <div className="App">
+    <div className="App">Recipe Notebook
     </div>
   );
 }
