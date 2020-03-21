7671240d0df1e4f7de354c0999683dc3573aec87
Karrie Dvorak <dvorak.karrie@gmail.com>
2020-03-21 13:41:23 -0500

    set up App as component and added component structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';                   // importing component
-import {Redirect, Route, Switch} from 'react-router-dom';   // importing router 
-import axios from 'axios';                                  // importing axios
+// import {Redirect, Route, Switch} from 'react-router-dom';   // importing router 
+// import axios from 'axios';                                  // importing axios
 
 import './App.css';                                         // importing css file
 
-import Home from './Components/Home';                        // importing Home component
-import User from './Components/User';                        // importing User component
-import NewRecipe from './Components/NewRecipe';              // importing NewRecipe component
-import EditRecipe from './components/EditRecipe';            // importing EditRecipe component
+// import Home from './Components/Home';                        // importing Home component
+// import User from './Components/User';                        // importing User component
+// import NewRecipe from './Components/NewRecipe';              // importing NewRecipe component
+// import EditRecipe from './Components/EditRecipe';            // importing EditRecipe component
 
-function App() {
-  return (
-    <div className="App">Recipe Notebook
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">Recipe Notebook
+      </div>
+    );
+  }
 }
 
 export default App;
