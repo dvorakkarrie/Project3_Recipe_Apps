e2c2aa6a747d296597f0fb29b617f17ca14f0aad
Karrie Dvorak <dvorak.karrie@gmail.com>
2020-03-25 09:31:27 -0500

    working on AuthorDetails component

--- frontend/src/Components/AuthorDetails.js
+++ frontend/src/Components/AuthorDetails.js
@@ -6,28 +6,29 @@ const AuthorDetails = props => {
     console.log(props.authors)
     console.log(props.recipes)
 
-    let authorDetail = props.authors.filter(author => 
+    let authorsDetail = props.authors.filter(author => 
         author._id === props.match.params.id)
-    let recipeDetail = props.recipes.filter(recipe => 
-        recipe._id === authorDetail.recipes)
+    let recipesDetail = props.recipes.filter(recipe => 
+        recipe.creator === authorsDetail[0]._id)
 
     // let authorsDetail = props.authors.find(
     //     author => author._id === props.match.params.id
     // )
 
     // let recipesDetail = props.recipes.find(
-    //     recipe => recipe._id === authorsDetail.recipes
+    //     recipe => recipe.creator === authorsDetail[0]._id
     //   )
-
+    console.log(authorsDetail)
+        console.log(recipesDetail)
     return (
         <div>
             <section>
-                <h2>Author: {authorDetail.name}</h2>
-                <h2>Email: {authorDetail.email}</h2>
+                <h2>Author: {authorsDetail.name}</h2>
+                <h2>Email: {authorsDetail.email}</h2>
                 <h2>List of Recipies</h2>
                 <h2>Recipe:  
-                    <Link to={`/recipes/${recipeDetail._id}`}>
-                    {recipeDetail.recipeName}
+                    <Link to={`/recipes/${recipesDetail}`}>
+                    {recipesDetail.recipeName}
                     </Link>
                 </h2>
             </section>        
